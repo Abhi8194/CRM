6a77a1e84e950e475562c1bc700697a18e0d97eb
Abhishek Mehra <abhimehra8194@gmail.com>
2024-09-27 18:37:41 +0530

    login and registration and logout done

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name=''),
     path('register',views.register,name='register'),
-    path('my-login', views.my_login,name='login')
+    path('my-login', views.my_login,name='login'),
+    path('user-logout',views.user_logout,name="user-logout"),
+    path('dashboard',views.dashboard,name='dashboard'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import *
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -17,6 +18,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     
     context = {'form':form}
     return render(request,'webapp/register.html',context=context)
@@ -33,6 +35,20 @@ def my_login(request):
             user = authenticate(request,username=username,password=password)
             if user is not None:
                 auth.login(request,user)
+                return redirect('dashboard')
    
     context = {'form2': form}
     return render(request,'webapp/my-login.html',context=context)
+
+
+# dasboard 
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request,'webapp/dashboard.html')
+
+
+# user logout
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect('login')
