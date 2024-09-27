567aac5e147b416cb8519b495cbc0210386576e4
Abhishek Mehra <abhimehra8194@gmail.com>
2024-09-27 18:19:27 +0530

    login and registration done

---
+++ webapp/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import PasswordInput,TextInput
+
+
+
+# Register or Create a User
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+# login form
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+
+
 
 def home(request):
     return render(request,'webapp/index.html')
+
+# register user
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {'form':form}
+    return render(request,'webapp/register.html',context=context)
+
+# login a user
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request,data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+   
+    context = {'form2': form}
+    return render(request,'webapp/my-login.html',context=context)
